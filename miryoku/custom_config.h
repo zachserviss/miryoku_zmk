117c8af3b46ef66d74461a90d5dd785f0fc04911
Zach Serviss <Zachary.Serviss@gov.ba.ca>
2024-10-05 13:19:49 -0700

    rename chocofi layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,24 +2,3 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_CLIPBOARD_WIN
-
-// custom_config.h
-
-#pragma once
-
-// Include the default Miryoku configuration
-#include "../miryoku/miryoku.h"
-
-// Define custom layer names
-#undef MIRYOKU_LAYER_LIST
-#define MIRYOKU_LAYER_LIST \
-  MIRYOKU_X(BASE,   "Alphas") \
-  MIRYOKU_X(EXTRA,  "Extra") \
-  MIRYOKU_X(TAP,    "Tap") \
-  MIRYOKU_X(BUTTON, "Button") \
-  MIRYOKU_X(NAV,    "Navigation") \
-  MIRYOKU_X(MOUSE,  "Mouse") \
-  MIRYOKU_X(MEDIA,  "Media") \
-  MIRYOKU_X(NUM,    "Numbers") \
-  MIRYOKU_X(SYM,    "Symbols") \
-  MIRYOKU_X(FUN,    "Functions")
