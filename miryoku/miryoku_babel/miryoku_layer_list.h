6429f56fc5517ee008bfdc5b7247e8d40ded45f5
Zach Serviss <zacharyserviss@proton.me>
2025-03-05 17:12:36 -0800

    readable layers

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -4,29 +4,29 @@
 
 #pragma once
 
-#if !defined (MIRYOKU_LAYER_LIST)
+#if !defined(MIRYOKU_LAYER_LIST)
 
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Colemak-DH") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Navigate") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Numbers") \
-MIRYOKU_X(SYM,    "Symbols") \
-MIRYOKU_X(FUN,    "Funtions")
+#define MIRYOKU_LAYER_LIST    \
+  MIRYOKU_X(BASE, "Alpha")    \
+  MIRYOKU_X(EXTRA, "Extra")   \
+  MIRYOKU_X(TAP, "Tap")       \
+  MIRYOKU_X(BUTTON, "Button") \
+  MIRYOKU_X(NAV, "Navigate")  \
+  MIRYOKU_X(MOUSE, "Mouse")   \
+  MIRYOKU_X(MEDIA, "Media")   \
+  MIRYOKU_X(NUM, "Numbers")   \
+  MIRYOKU_X(SYM, "Symbols")   \
+  MIRYOKU_X(FUN, "Funtions")
 
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
+#define U_BASE 0
+#define U_EXTRA 1
+#define U_TAP 2
 #define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
+#define U_NAV 4
+#define U_MOUSE 5
+#define U_MEDIA 6
+#define U_NUM 7
+#define U_SYM 8
+#define U_FUN 9
 
 #endif
